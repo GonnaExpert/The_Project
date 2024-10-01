515dc2d93b6002dd7b336bfed3fc7016ce3da65c
Dmitry For Justice <64169042+GonnaExpert@users.noreply.github.com>
2024-10-01 19:20:49 +0300

    Update slave_all.sh

--- Backend/slave_all.sh
+++ Backend/slave_all.sh
@@ -22,10 +22,18 @@ echo ''
 echo "***** Восстанавливаем БД для WIKI на SLAVE *****"
 echo ''
 
+#Установка московского времени 
+timedatectl set-timezone Europe/Moscow
+echo 'Установлена единая в инфраструктуре временная зона (Европа, Москва) - Ok'
+#проверить  какое сейчас 
+echo "Время на $(hostname)" 
+date 
+echo '' 
+
 #рестартую mysql сервис.
-for service in mysql
+for service in  prometheus-node-exporter mysql
 do
-  echo "--- Ожидание - рестарт $service"
+  echo "--- Ожидание - рестарт   $service"
   echo ''
   systemctl restart $service
   systemctl status $service | grep active > /dev/null 2>&1 
