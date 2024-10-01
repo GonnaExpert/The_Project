63f03556a3378bca6462565892c0667f2d224b42
Dmitry For Justice <64169042+GonnaExpert@users.noreply.github.com>
2024-10-01 18:13:31 +0300

    Update monitoring.sh

--- Control/monitoring.sh
+++ Control/monitoring.sh
@@ -12,39 +12,47 @@ gitDirName='/root/The_Project_git_repo'
 typeConfigDirName='Control'
 gitTypeDirName=$gitDirName/$typeConfigDirName
 
+#Установка московского времени 
+timedatectl set-timezone Europe/Moscow
+#echo 'Установлена временная зона Москва - Ok'
+#проверить  какое сейчас 
+timedatectl status
+date 
+echo '' 
 
 
-
-
-#рестартуем сервисы с проверкой
-for service in prometheus prometheus-node-exporter grafana-server
-do
-  echo "--- Ожидание - рестарт $service"
-  echo ''
-  systemctl restart $service
-  systemctl status $service | grep active > /dev/null 2>&1 
-  if [ $? -eq 0 ]
-  then
-      echo "Рестарт $service - OK"
-      echo '' 
-  else
-      echo " (!) $service не запустился"
-	    echo '!! Скрипт прерывает работу !!'
-      exit 11
-  fi
-done 
-
-# вносим изменения в файл конфигурации Прометея
-
+#1. вносим изменения в файл конфигурации Прометея
 cpFileName='prometheus.yml' 
 
-#1. Копир. файл конфиг вместо имеющегося
-#здесь - файл конфиги Прометея yml с настроенными нодами
+# Копир. файл конфиг вместо имеющегося
 sourceFile=$gitTypeDirName/$cpFileName
 destinDir=/etc/prometheus
 destinFile=$destinDir/$cpFileName
-description="конфигурация сервера Prometheus"
+description="конфигурации сервиса Prometheus"
+if [ -f $destinFile ]
+then 
+  mv $destinFile $destinFile.bak
+fi 
 
+if cp -f $sourceFile $destinFile 
+then 
+ echo "Изменение $description - OK"
+ echo ''
+else 
+ echo " (!) Изменение $description не прошло"
+ echo '!! Скрипт прерывает работу !!'
+ exit 12
+fi 
+
+
+#2. вносим изменения в файл конфигурации Графана
+cpFileName='prometheus.yml' 
+
+#Копир. файл конфиг вместо имеющегося
+sourceFile=$gitTypeDirName/$cpFileName
+destinDir=/etc/prometheus
+destinFile=$destinDir/$cpFileName
+description="конфигурации сервиса Grafana"
 if [ -f $destinFile ]
 then 
   mv $destinFile $destinFile.bak
@@ -62,7 +70,7 @@ fi
 
 
 #рестартуем сервисы с проверкой
-for service in prometheus
+for service in  prometheus-node-exporter prometheus  grafana-server
 do
   echo "--- Ожидание - рестарт $service"
   echo ''
